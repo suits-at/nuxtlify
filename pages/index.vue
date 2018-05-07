f750797758341a31b96b568bf5cf0c373b4c86cb
suits-at <office@suits.at>
2018-05-07 21:53:59 +0200

    add projects

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,14 @@
         </nuxt-link>
       </li>
     </ul>
+    <h2>Projects</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.date">
+        <nuxt-link :to="project._path">
+          {{ project.title }}
+        </nuxt-link>
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -27,7 +35,15 @@ export default {
       _path: `/blog/${key.replace('.json', '').replace('./', '')}`
     }));
 
-    return { posts };
+    // Using webpacks context to gather all files from a folder
+    const project_context = require.context('~/content/projects/project/', false, /\.json$/);
+
+    const projects = project_context.keys().map(key => ({
+      ...project_context(key),
+      _path: `/projects/${key.replace('.json', '').replace('./', '')}`
+    }));
+
+    return { posts, projects };
   }
 };
 </script>
@@ -39,27 +55,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
---
+++ pages/projects/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <h1>{{ title }}</h1>
+    <p>created: {{ date }}</p>
+    <p>{{ body }}</p>
+    <img :src="thumbnail" />
+
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    return await import('~/content/projects/project/' + params.slug + '.json');
+  }
+};
+</script>
